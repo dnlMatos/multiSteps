8131647d88bf8355640607c0fe1f3034f6e1d143
dnlMatos <georginedanilO@gmail.com>
2023-01-26 18:17:01 -0300

    step3

--- src/components/step3/Step3.jsx
+++ src/components/step3/Step3.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Step3() {
+import FormJSON from "../../form.json";
+import Step from "../step";
+
+import * as S from "./style";
+
+const { step3 } = FormJSON;
+
+function Step3({ onStepSubmit, formData, ...props }) {
+  const [selectedAddons, setSelectedAddons] = useState(
+    formData.step3.selectedAddons ?? []
+  );
+
+  const { billingType } = formData.step2;
+
+  const changeSelectedAddons = (checked, selectedAddon) => {
+    if (checked) {
+      setSelectedAddons([...selectedAddons, selectedAddon]);
+    } else {
+      setSelectedAddons(
+        selectedAddons.filter((addon) => addon.id !== selectedAddon.id)
+      );
+    }
+  };
+
+  const checkSelected = (id) => selectedAddons.some((i) => i.id === id);
+
+  const onSubmit = () => {
+    onStepSubmit("step3", "step4", {
+      selectedAddons,
+    });
+  };
+
   return (
-    <div>Step3</div>
-  )
+    <Step {...props} handleSubmit={onSubmit}>
+      <S.Step3>
+        {step3[billingType].map((item) => (
+          <S.Item key={item.id} isSelected={checkSelected(item.id)}>
+            <S.Input
+              defaultChecked={checkSelected(item.id)}
+              type="checkbox"
+              onChange={(e) => changeSelectedAddons(e.target.checked, item)}
+            />
+            <S.InputBody>
+              <S.Title>{item.title}</S.Title>
+              <S.Subtitle>{item.description}</S.Subtitle>
+            </S.InputBody>
+            <S.Price>{item.price}</S.Price>
+          </S.Item>
+        ))}
+      </S.Step3>
+    </Step>
+  );
 }
+
+export default Step3;
--- src/components/step3/style.jsx
+++ src/components/step3/style.jsx
@@ -1,12 +1,46 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const MultiStepForm = styled.div`
+export const Step3 = styled.div`
   display: flex;
-  gap: 12px;
-  padding: 12px;
-  background-color: var(--white);
-  border-radius: 16px;
-  height: 100%;
-  width: 100%;
-  box-shadow: 0 5px 10px 10px rgba(0, 0, 0, 0.05);
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Item = styled.label`
+  display: flex;
+  align-items: center;
+  padding: 24px;
+  gap: 16px;
+  border: 1px solid var(--light-gray);
+  border-radius: 8px;
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      background-color: var(--alabester);
+      border-color: var(--purplish-blue);
+    `}
+  &:hover {
+    border-color: var(--purplish-blue);
+  }
+`;
+
+export const Input = styled.input``;
+
+export const InputBody = styled.div`
+  flex: 1;
+`;
+
+export const Title = styled.h4`
+  font-weight: 500;
+  margin-bottom: 4px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 14px;
+  color: var(--cool-gray);
+`;
+
+export const Price = styled.p`
+  color: var(--purplish-blue);
+  font-weight: 500;
 `;
